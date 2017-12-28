f7057d64f7a39200cc26ffb6d0e4fb58c78ab80e
jjkusowski <jjkusowski@yahoo.com>
2017-12-27 16:16:09 -0800

    bst remove works and tested

--- lab-jeff/__test__/binary-search-tree.test.js
+++ lab-jeff/__test__/binary-search-tree.test.js
@@ -2,15 +2,78 @@
 
 const BinarySearchTree = require('../lib/binary-search-tree');
 
-let bst = new BinarySearchTree(10);
-bst.insert(5);
-bst.insert(2);
-bst.insert(8);
-bst.insert(16);
+
 
 
 describe('remove', () => {
-  test('should remove the node with the value passed in and restructure the bst', () => {
-    expect(bst.remove)
+  test('Node removed has no children.  Should remove reference from parent node.', () => {
+    let bst = new BinarySearchTree(10);
+    bst.insert(5);
+    bst.insert(2);
+    bst.insert(8);
+    bst.insert(16);
+    bst.remove(16);
+
+    expect(bst.right).toEqual(null);
+  });
+
+  test('Node removed has 1 child.  Parent node should now reference removed node\'s child', () => {
+    let bst = new BinarySearchTree(10);
+    bst.insert(5);
+    bst.insert(2);
+    bst.insert(8);
+    bst.insert(16);
+    bst.insert(14);
+    expect(bst.right.value).toEqual(16);
+    bst.remove(16);
+
+    expect(bst.right.value).toEqual(14);
+
+
+  });
+
+  test('Node removed has 2 children.  Should find the smallest value on the removed node\'s right tree, replace it\'s value, and remove the node that moved', () => {
+    let bst = new BinarySearchTree(10);
+    bst.insert(5);
+    bst.insert(2);
+    bst.insert(8);
+    bst.insert(16);
+    expect(bst.left.value).toEqual(5);
+    bst.remove(5);
+    expect(bst.left.value).toEqual(8);
+
+  });
+  test('Node to be removed is the root and has no children', () => {
+    let bst = new BinarySearchTree(10);
+    bst.remove(10);
+    expect(bst.value).toBeNull();
+  });
+
+  test('Node to be removed is the root and has 1 child', () => {
+    let bst = new BinarySearchTree(10);
+    bst.insert(5);
+    bst.insert(2);
+    bst.remove(10);
+    expect(bst.value).toEqual(5);
+    expect(bst.left.value).toEqual(2);
+  });
+
+  test('Node to be removed is the root and has 2 children', () => {
+    let bst = new BinarySearchTree(10);
+    bst.insert(5);
+    bst.insert(2);
+    bst.insert(8);
+    bst.insert(16);
+    bst.remove(10);
+    expect(bst.value).toEqual(16);
+    expect(bst.right).toBeNull();
+  });
+  test('value to be removed is not in the tree. Should return null', () => {
+    let bst = new BinarySearchTree(10);
+    bst.insert(5);
+    bst.insert(2);
+    bst.insert(8);
+    bst.insert(16);
+    expect(bst.remove(100)).toBeNull();
   });
 });
--- lab-jeff/lib/binary-search-tree.js
+++ lab-jeff/lib/binary-search-tree.js
@@ -47,23 +47,119 @@ class BinarySearchTree{
       return null;
   }
 
-  remove(value, node){
-    if(!node) node = this;
+  findMin(node){
+    while(node.left)
+      node = node.left;
+    return node;
+  }
 
-    if(value < node.value){
-      if(node.left){
-        this.remove(value, node.left); // might be node.remove?
-      }
-    } else if (value > node.value){
-      if(node.right){
-        this.remove(value, node.right);
-      }
-    } else {
-      if(!node.left && !node.right)
-        node = null;
+  findParent(value, root){
 
-      
+    console.log('root: ', root);
+
+    if(value === root.value){
+      return 'root';
     }
+    if(value < this.value){
+      if(this.left.value === value){
+        return this;
+      }
+      return this.left.findParent(value);
+    }
+    if(this.right.value === value){
+      return this;
+    }
+    return this.right.findParent(value);
+
+  }
+
+  remove(value, root = null){
+    let parent;
+    if(root === null) {
+      root = this;
+    }
+    if(!this.find(value)){
+      return null;
+    }
+
+    if(this.value === value){ //value is found
+
+      if(this === root){ //node to be removed is the root
+        console.log('node to remove is root');
+        if(this.right && this.left){//root to be removed and has 2 children
+          let minRight = this.findMin(this.right);
+          this.value = minRight.value;
+          root = this;
+          if(this.right === minRight){
+            this.right = null;
+            return;
+          }
+          return minRight.remove(minRight.value, root);
+        } else if(this.right || this.left){//root to be removed and has 1 child
+          if(this.right){
+            this.value = this.right.value;
+            this.right.remove(this.right.value);
+            return;
+          } else {
+            this.value = this.left.value;
+            this.left.remove(this.left.value);
+            return;
+          }
+        } else{//root to be removed and has no children
+          root = null;
+          this.value = null;
+          return;
+        }
+      } else { //if the node to be removed is not the root
+
+
+        console.log('value found.  node = ', this);
+        parent = root.findParent(value, root);
+        console.log('parent: ', parent);
+        if(this.right && this.left){ //node to remove has 2 children
+          let minRight = this.findMin(this.right);
+          this.value = minRight.value;
+          return minRight.remove(minRight.value, root);
+        } else if (this.right || this.left){ // node to remove has 1 child
+          if(parent.left === this){
+            if(this.left){
+              parent.left = this.left;
+              return;
+            } else {
+              parent.left = this.right;
+              return;
+            }
+          } else {
+            if(this.left){
+              parent.right = this.left;
+              return;
+            } else {
+              parent.right = this.right;
+              return;
+            }
+          }
+        } else {//node to remove has no children
+          if(parent.left === this){
+            parent.left = null;
+            this.value = null;
+            return;
+
+          } else {
+            parent.right = null;
+            return;
+          }
+
+        }
+
+      }
+
+    }
+    if(value < this.value)
+      return this.left.remove(value, root);
+
+    return this.right.remove(value, root);
+
+
   }
 }
 module.exports = BinarySearchTree;
